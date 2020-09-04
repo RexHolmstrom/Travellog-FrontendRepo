453eef46807a957aa51684329ae2754a04be5b0b
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-04 21:27:49 +0200

    API update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMapGL from "react-map-gl";
+import { listLogEntries } from "./API";
 
 const App = () => {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 37.7577,
-    longitid: -122.4376,
-    zoom: 8,
+    latitude: 55.1736,
+    longitid: -23.8948,
+    zoom: 3,
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      console.log(logEntries);
+    })();
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/rexholmstrom/ckeom0yvs4d1r1ankxvt3lcch"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
     />
