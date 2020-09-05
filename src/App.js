233dffec01371018bbeae55f21bcf799d8bf7c5d
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-05 18:32:09 +0200

    update icon map

--- src/App.js
+++ src/App.js
@@ -31,10 +31,28 @@ const App = () => {
           key={entry._id}
           latitude={entry.latitude}
           longitude={entry.longitid}
-          offsetLeft={-20}
-          offsetTop={-10}
+          offsetLeft={-12}
+          offsetTop={-24}
         >
-          <div>{entry.title}</div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                width: "24px",
+                height: "24px",
+              }}
+              fill="none"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path
+                className="marker"
+                d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"
+              />
+              <circle cx="12" cy="10" r="3" />
+            </svg>
+          </div>
         </Marker>
       ))}
     </ReactMapGL>
