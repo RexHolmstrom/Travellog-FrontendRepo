fdde211698a971ca04b2b19c7f046d90ae2fe018
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-05 21:10:12 +0200

    onclick add

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { listLogEntries } from "./API";
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowpopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
     latitude: 55.1736,
-    longitid: -23.8948,
+    longitude: -23.8948,
     zoom: 3,
   });
 
@@ -20,19 +21,28 @@ const App = () => {
     })();
   }, []);
 
+  const showAddMarkerPopup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
+  };
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/rexholmstrom/ckeom0yvs4d1r1ankxvt3lcch"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
+      onDblClick={showAddMarkerPopup}
     >
       {logEntries.map((entry) => (
         <>
           <Marker
             key={entry._id}
             latitude={entry.latitude}
-            longitude={entry.longitid}
+            longitude={entry.longitude}
 
             // offsetLeft={-12}
             // offsetTop={-24}
@@ -59,20 +69,25 @@ const App = () => {
           {showPopup[entry._id] ? (
             <Popup
               latitude={entry.latitude}
-              longitude={entry.longitid}
+              longitude={entry.longitude}
               closeButton={true}
               closeOnClick={true}
+              dynamicPosition={true}
               onClose={() => setShowpopup({})}
               anchor="top"
             >
               <div className="popup">
                 <h3>{entry.title}</h3>
                 <p>{entry.comments}</p>
+                <small>
+                  Visited on: {new Date(entry.visitDate).toLocaleDateString()}
+                </small>
               </div>
             </Popup>
           ) : null}
         </>
       ))}
+      {addEntryLocation ? <></> : null}
     </ReactMapGL>
   );
 };
