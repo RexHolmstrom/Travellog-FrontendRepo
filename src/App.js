b86c39cf2a551d12631ce19b19852fc1fce8087b
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-05 19:14:53 +0200

    update icon map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { listLogEntries } from "./API";
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowpopup] = useState({});
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -27,36 +28,51 @@ const App = () => {
       onViewportChange={setViewport}
     >
       {logEntries.map((entry) => (
-        <Marker
-          key={entry._id}
-          latitude={entry.latitude}
-          longitude={entry.longitid}
-          // offsetLeft={-12}
-          // offsetTop={-24}
-        >
-          <div>
-            <img
-              className="marker"
-              style={{
-                height: `${6 * viewport.zoom}px`,
-                width: `${6 * viewport.zoom}px`,
-              }}
-              src="https://i.imgur.com/y0G5YTX.png"
-              alt="marker"
-            />
-          </div>
-        </Marker>
+        <>
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitid}
+
+            // offsetLeft={-12}
+            // offsetTop={-24}
+          >
+            <div
+              onClick={() =>
+                setShowpopup({
+                  ...showPopup,
+                  [entry._id]: true,
+                })
+              }
+            >
+              <img
+                className="marker"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                src="https://i.imgur.com/y0G5YTX.png"
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitid}
+              closeButton={true}
+              closeOnClick={true}
+              onClose={() => this.setState({ showPopup: false })}
+              anchor="top"
+            >
+              <div>
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+              </div>
+            </Popup>
+          ) : null}
+        </>
       ))}
-      <Popup
-        latitude={37.78}
-        longitude={-122.41}
-        closeButton={true}
-        closeOnClick={true}
-        onClose={() => this.setState({ showPopup: false })}
-        anchor="top"
-      >
-        <div>You are here!</div>
-      </Popup>
     </ReactMapGL>
   );
 };
